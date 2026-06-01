61f98c72584e1d03b5acc335ff3c2dd78d62a2c1
Alexander Ruff <ajruff94@gmail.com>
2026-06-01 15:04:47 +0100

    test: add shared constants helper

---
+++ ti4-companion-web/tests/helpers/constants.js
@@ -0,0 +1,8 @@
+// tests/helpers/constants.js
+export const USER_ID    = 'user-uuid'
+export const GAME_ID    = 'game-uuid'
+export const PLAYER_ID  = 'player-uuid'
+export const OPPONENT_ID = 'opponent-uuid'
+export const COMBAT_ID  = 'combat-uuid'
+export const TILE_ID    = 'tile-uuid'
+export const SYSTEM_KEY = '1,-1'
